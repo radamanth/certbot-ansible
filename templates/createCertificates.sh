ccc029ef86809b7261fc9729793701d3a16a6563
Tony Deboschère <radamanth@gmail.com>
2017-08-15 16:59:14 +0200

    Non interactive create script with domain mail for alerts

--- templates/createCertificates.sh
+++ templates/createCertificates.sh
@@ -5,7 +5,7 @@ sudo service apache2 stop
 
 {% for domain  in domain_list  %}
 
-{{certbot_install_path}}/certbot-auto certonly -tvv --standalone --renew-by-default  {% for sub in domain.list %} -d {{sub}} {% endfor %}
+{{certbot_install_path}}/certbot-auto certonly -m "{{certbot_mail}}" -tvv -n --agree-tos --standalone --renew-by-default  {% for sub in domain.list %} -d {{sub}} {% endfor %}
 
 {% endfor %}
 
